69b90036946f83b608a4d5235a537e22e80ab68b
Dan Ben-Natan <dbn290895@gmail.com>
2024-11-19 17:44:16 +0200

    eslint and prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['universe/native'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': 'error',
+    'react/jsx-uses-vars': 'error',
+    // 'unused-imports/no-unused-imports': 'error',
+    'no-nested-ternary': 'error',
+    'import/prefer-default-export': 'error',
+  },
 };
