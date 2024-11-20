3c77759c18c550921b3ee0d552149aa075ac9b9c
Dan Ben-Natan <dbn290895@gmail.com>
2024-11-20 12:38:38 +0200

    EL-12 missing configuration for paper

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,19 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  env: {
-    production: {
-      plugins: ['react-native-paper/babel'],
-    },
-  },
+  plugins: [
+    'react-native-paper/babel',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@src': './src',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@lib': './src/lib',
+        },
+      },
+    ],
+  ],
 };
+
--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,12 +1,15 @@
 import { ThemeContext } from '@src/context/ThemeContext';
 import { darkTheme, lightTheme } from '@src/styles/theme';
 import React, { useContext } from 'react';
+import { SafeAreaView } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 const Providers = ({ children }: React.PropsWithChildren) => {
   const { theme } = useContext(ThemeContext);
   return (
-    <PaperProvider theme={theme === 'light' ? lightTheme : darkTheme}>{children}</PaperProvider>
+    <PaperProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <SafeAreaView>{children}</SafeAreaView>
+    </PaperProvider>
   );
 };
 
